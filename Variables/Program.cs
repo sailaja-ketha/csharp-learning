c5a9f9f743366721c24dd0d4c8cf2d90d9943b9e
sailaja-ketha <128355746+sailaja-ketha@users.noreply.github.com>
2023-10-10 15:51:21 +0530

    Updated

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,7 +1,7 @@
 ﻿//variables are nothing but store the data of different data types
 // there are two types of variables local variable and global variable
-// local variable can be accessed only with the function
-//global variable can be accessed with in class
+// local variable are generally declared  with the function
+//global variable can be accessed with in the class
 class Variables
 {
     //global variables
@@ -21,10 +21,10 @@ class Variables
 
     static void Main(string[] args)
     {
-        Variables v = new Variables();
-        v.EmployeeId( 40);
+        Variables variables = new Variables();
+        variables.EmployeeId( 40);
         Console.WriteLine("Variables: of different data types");
-        Console.WriteLine(v.employeeId + "\t" + v.name + "\t" + v. height + "\t"+ v.firstLetterOfName+"\t" + v.status) ;
+        Console.WriteLine(variables.employeeId + "\t" + variables.name + "\t" + variables. height + "\t"+ variables.firstLetterOfName+"\t" + variables.status) ;
     }
 
 
--- data-types/Program.cs
+++ data-types/Program.cs
@@ -1,1 +1,77 @@
-﻿
+﻿//Datatype specifies type and size of variable
+//we must declare the type of variable that indicates which type of data is storing
+
+class DataTypes
+{
+    int studentId = 1;
+    long phoneNumber = 9087654321l;
+    double weight = 40.58d;
+    float height = 2.39f;
+    char lastLetterOfName= 'u';
+    bool status = true;
+    string name = "Sailu";
+
+    public void PrintStudentDetails()
+    {
+        Console.WriteLine("Name of the student is : " + name);
+        Console.WriteLine("Id of the student is : " + studentId);
+        Console.WriteLine("Phonenumber of the student is : " + phoneNumber);
+        Console.WriteLine("last letter of the student is : " + lastLetterOfName);
+        Console.WriteLine("Educational status  is  : " + status);
+        Console.WriteLine("Weight is : " + weight);
+        Console.WriteLine("Height is : " + height);
+    }
+
+    static void Main(string[] args)
+    {
+        DataTypes dataTypes = new DataTypes();
+        Console.WriteLine("Data Types");
+        dataTypes.PrintStudentDetails();
+
+        TypeCasting typeCasting = new TypeCasting();
+        Console.WriteLine("Type casting");
+        typeCasting.ConvertedDataTypes();
+        typeCasting.ExplicitTypeCasting();
+    }
+
+
+    //typecasting//
+    //1.Implicit 2.Explicit
+    //1.Implicit typecasting is converting automatically  char --> int-->long-->float-->double
+    //2.Explicit convert one type to another manually double-->float-->long-->int-->char
+
+
+    class TypeCasting 
+    {
+        //Implicit type Casting
+        public static int age = 21;
+        public static double convertedAge = age;
+
+        public static char letter = 'a';
+        public static int convertedLetter = letter;
+
+        public static long phoneNumber = 9087654321l;
+        public static float convertedPhoneNumber= phoneNumber;
+
+
+
+        public void ConvertedDataTypes()
+        {
+            Console.WriteLine("Converted age is : " + convertedAge);
+            Console.WriteLine("Converted letter is : " + convertedLetter);
+            Console.WriteLine("Converted phonenumber is : " + convertedPhoneNumber);
+        }
+
+        //Explicit type casting
+        public void ExplicitTypeCasting()
+        {
+            Console.WriteLine( Convert.ToInt32(convertedAge));
+            Console.WriteLine(Convert.ToChar(convertedAge));
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+        }
+    }
+
+
+}

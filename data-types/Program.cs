687cbebb6584957d9dbba4d205bba0ca5a22e051
sailaja-ketha <128355746+sailaja-ketha@users.noreply.github.com>
2023-10-10 15:51:33 +0530

    Updated

--- data-types/Program.cs
+++ data-types/Program.cs
@@ -66,7 +66,6 @@ class DataTypes
         public void ExplicitTypeCasting()
         {
             Console.WriteLine( Convert.ToInt32(convertedAge));
-            Console.WriteLine(Convert.ToChar(convertedAge));
             Console.WriteLine("Enter your age:");
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your age is: " + age);

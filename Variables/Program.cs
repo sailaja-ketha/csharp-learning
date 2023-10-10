07b0a375e456d505b6e852ad69d7e892caa4e2ee
sailaja-ketha <128355746+sailaja-ketha@users.noreply.github.com>
2023-10-10 13:13:03 +0530

    Updated

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//variables are nothing but store the data of different data types
+// there are two types of variables local variable and global variable
+// local variable can be accessed only with the function
+//global variable can be accessed with in class
+class Variables
+{
+    //global variables
+    int employeeId = 40;
+    string name = "sailaja";
+    double height = 5.2;
+    char firstLetterOfName = 'S';
+    bool status = true;
+
+    //Local variables
+    public void EmployeeId( int employeeId)
+    {
+        int Id;
+        Id= employeeId;
+        Console.WriteLine("This is the Local variable" + Id);
+    }
+
+    static void Main(string[] args)
+    {
+        Variables v = new Variables();
+        v.EmployeeId( 40);
+        Console.WriteLine("Variables: of different data types");
+        Console.WriteLine(v.employeeId + "\t" + v.name + "\t" + v. height + "\t"+ v.firstLetterOfName+"\t" + v.status) ;
+    }
+
+
+}

0fe2471e9eb8471b00106c732ccc01908055b249
sailaja-ketha <128355746+sailaja-ketha@users.noreply.github.com>
2023-10-12 19:05:20 +0530

    Updated

---
+++ Exceptions/Exceptions.cs
@@ -0,0 +1,86 @@
+﻿
+
+namespace Exceptions
+{
+    public class Exceptions
+    {
+         string[] colors = { "Red", "Green", "Pink" };
+
+         public void IndexOutOfRangeException()
+         {
+            try 
+            {
+                Console.WriteLine(colors[3]);
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+               Console.WriteLine("Finally block executed.");
+            }
+
+        }
+
+        public void FoormatException()
+        {
+
+            string input = "abc";
+            try
+            {
+                int number = int.Parse(input);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("FormatException caught: " + ex.Message);
+            }
+
+        }
+
+        public void DivideByZeroException()
+        {
+            int result;
+            try
+            {
+                int a = 10;
+                result = a / 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DividedByZeroException caught: " + ex.Message);
+            }
+        }
+
+
+        public void UserInputException()
+        {
+            try
+            {
+                Console.WriteLine("Enter Number");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void NullReferenceException()
+        {
+            string str = null;
+            try
+            {
+                int length = str.Length;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("NullReferenceException caught: " + ex.Message);
+            }
+
+        }
+    }
+
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿//Exception is a problem that raises during execution of the program
+using System;
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*****Exception Handling****");
+            Exceptions exception = new Exceptions(); 
+            exception.IndexOutOfRangeException(); 
+            exception.FoormatException();
+            exception.DivideByZeroException();
+            exception.UserInputException();
+            exception.NullReferenceException();
+
+
+
+        }
+    }
+}

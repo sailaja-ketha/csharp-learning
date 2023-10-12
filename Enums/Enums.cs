734cbe0025d9bc77db6de618d35945095bb10634
sailaja-ketha <128355746+sailaja-ketha@users.noreply.github.com>
2023-10-12 15:56:15 +0530

    Updated

---
+++ Enums/Enums.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Enums
+{
+    public  class Enums
+    {
+        enum Fruits
+        {
+            Mango,
+            Grapes,
+            Apple,
+            Orange,
+            Banana
+        }
+
+        enum Days
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+        public static void DisplayFruitsData()
+        {
+            Console.WriteLine(Fruits.Mango);
+            Console.WriteLine(Fruits.Grapes);
+            Console.WriteLine(Fruits.Apple);
+            Console.WriteLine(Fruits.Orange);
+            Console.WriteLine(Fruits.Banana);
+            Console.WriteLine((int)(Fruits.Mango));
+        }
+
+        public void DisplayDays()
+        {
+            Console.WriteLine(Days.Monday);
+            Console.WriteLine(Days.Tuesday);
+            Console.WriteLine(Days.Wednesday);
+            Console.WriteLine(Days.Thursday);
+            Days currentDay = Days.Thursday;
+            if(currentDay == Days.Thursday)
+            {
+                Console.WriteLine("Current day is : " + currentDay);
+
+            }
+
+        }
+    }
+}
---
+++ Enums/Program.cs
@@ -0,0 +1,15 @@
+﻿//An enum is a special class that represents a group of constants
+//Use enums when you have values that you know aren't going to change
+namespace Enums
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Enums.DisplayFruitsData();
+            Enums enums=new Enums();
+            enums.DisplayDays();
+        }
+    }
+}

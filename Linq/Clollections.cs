a234820e000e1c176a85922c9a08ef35ad4c3661
sailaja-ketha <128355746+sailaja-ketha@users.noreply.github.com>
2023-10-20 19:49:40 +0530

    Updated

---
+++ Linq/Clollections.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Linq
+{
+    public class Collections
+    {
+        public static void Where()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+            foreach (var n in evenNumbers)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+
+        public static void OrderByMethods()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var sortedNumbers = numbers.OrderBy(n => n); // Ascending order
+            var reverseSortedNumbers = numbers.OrderByDescending(n => n); // Descending order
+            Console.WriteLine("Ascending order");
+            foreach (var n in sortedNumbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("Descending order");
+            foreach (var n in reverseSortedNumbers)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+
+        public static void Aggreration()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
+            var average = numbers.Average();
+            Console.WriteLine(average);
+            var count = numbers.Count();
+            Console.WriteLine(count);
+
+        }
+
+
+        public static void Concat()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var otherNumbers = new List<int> { 5, 6, 7, 8, 9, 10, 11, 12 };
+            var combinedNumbers = numbers.Concat(otherNumbers);
+            foreach( var number in combinedNumbers )
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,5 +1,25 @@
-﻿//Linq language integrated query
-static void Main(string[] args)
+﻿
+using System.Runtime.InteropServices;
+
+namespace Linq
 {
-    Console.WriteLine("***Linq***");
+
+    //Linq language integrated query
+    //using linq we can write queries
+    //LINQ is a structured query syntax built in C# and VB.NET to retrieve data from
+    //different types of data sources such as collections, ADO.Net DataSet, XML Docs,
+    //web service and MS SQL Server and other databases
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Linq***");
+            Collections.Where();
+            Collections.OrderByMethods();
+            Collections.Aggreration();
+            Collections.Concat();
+
+        }
+    }
+
 }
